8b1b0afe977bcd98c99d7ff42ce992a40fb1b9c4
DimiVaksman <vde92@hotmail.com>
2023-02-17 19:03:43 +0100

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,38 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+console.log(galleryContainer);
+
+function onImgItems(items){
+    return items
+    .map((item) => `<div class="gallery">
+    <a class="gallery__item" href="${item.original}"><img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}"/></a>
+</div>`).join("");
+    }
+
+
+
+const addGalleryItems = onImgItems(galleryItems);
+galleryContainer.innerHTML = addGalleryItems;
+
+
+
+
+galleryContainer.addEventListener('click' , openModalperClick)
+
+
+function openModalperClick (e){
+    e.preventDefault()
+
+    if( e.target.nodeName !== 'IMG'){
+    return
+    }
+
+    new SimpleLightbox('.gallery a', {
+        captionDelay: 250,
+     });
+
+}
